3fa50bec392fbfbb10d99f365519d583611d12ea
rich5k <richardanatsui@gmail.com>
2022-04-01 14:51:59 +0100

    created basic structure for messages page

--- view/messages.php
+++ view/messages.php
@@ -10,14 +10,54 @@
     <div class="grid grid-cols-3">
         <div class="contacts">
             <div class="profile">
-                
+                My Profile
+            </div>
+            <div class="matches-page">
+
+            </div>
+            <div class="messages">
+                <div class="message">
+
+                </div>
             </div>
         </div>
         <div class="current-chat">
-
+            <div class="chat-header">
+                CONVERSATION WITH ALEX <br>
+                Alex Liked you on 09/12/16
+                <button class="close-chat"></button>
+            </div>
+            <div class="chat-window">
+                <div class="your-text">
+                    <span class="datetime">
+                        Sept 12, 2016 11:11PM
+                    </span>
+                    <div class="your-chat-bubble">
+                        Hey, we matched!
+                    </div>
+                    <span class="text-status">Sent</span>
+                </div>
+                <div class="your-match-text">
+                    Hey, Jess. How are you?
+                </div>
+            </div>
         </div>
         <div class="contact-info">
-            
+            <div class="your-match-pics">
+                <img src="" alt="Alex's pic">
+            </div>
+            <div class="personal-info">
+                <span class="name-age">Alex, 24</span>
+                <span class="your-match-title">Founder at Creative Productions</span>
+            </div>
+            <div class="your-match-bio">
+                <p>
+                    I'm a libra, but not sure what it means. I like 
+                    leftover pizza better than the fresh stuff. I love
+                    to travel. I have a cat named pickles, if he likes
+                    you I probably will too.
+                </p>
+            </div>
         </div>
     </div>
 </body>
