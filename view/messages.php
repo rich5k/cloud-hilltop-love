93442005ebbddae696ba34368c2b7b43abfcae5c
rich5k <richardanatsui@gmail.com>
2022-04-05 15:09:49 +0100

    modified page structure and styles and have to fix page margin problem

--- view/messages.php
+++ view/messages.php
@@ -5,24 +5,39 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Your Messages</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/messages.css">
 </head>
 <body>
-    <div class="grid grid-cols-3">
-        <div class="contacts">
-            <div class="profile">
-                My Profile
+    <div class="row">
+        <div class="contacts col-lg-4">
+            <div class="profile-banner py-2 px-4">
+                <img src="../images/homepic6.jpg" alt="profile-pic"> <strong>My Profile</strong>
+                <span class="search-button">
+                    <button><i class="fa-solid fa-magnifying-glass"></i></button>
+                </span>
             </div>
             <div class="matches-page">
-
+                <div class="caption">
+                    Discover New Matches
+                </div>
+                <div class="micro-caption">
+                    Start swiping to start connecting with new people
+                </div>
             </div>
             <div class="messages">
+                <div class="messages-title">
+                    Messages
+                </div>
                 <div class="message">
 
                 </div>
             </div>
         </div>
-        <div class="current-chat">
+        <div class="current-chat col-lg-6">
             <div class="chat-header">
+                <img src="../images/person1.jpg" alt="Alex's pic">
                 CONVERSATION WITH ALEX <br>
                 Alex Liked you on 09/12/16
                 <button class="close-chat"></button>
@@ -40,16 +55,21 @@
                 <div class="your-match-text">
                     Hey, Jess. How are you?
                 </div>
+                <form action="">
+                    <input type="text">
+                    <button type="submit">SEND</button>
+                </form>
             </div>
         </div>
-        <div class="contact-info">
+        <div class="contact-info col-lg-2">
             <div class="your-match-pics">
-                <img src="" alt="Alex's pic">
+                <img src="../images/person1.jpg" alt="Alex's pic">
             </div>
             <div class="personal-info">
-                <span class="name-age">Alex, 24</span>
+                <span class="name-age p-2">Alex, 24</span>
                 <span class="your-match-title">Founder at Creative Productions</span>
             </div>
+            <br><br>
             <div class="your-match-bio">
                 <p>
                     I'm a libra, but not sure what it means. I like 
