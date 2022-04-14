56309a186be634fa5459fcc83445d22a7763dd0b
kwakuAgyemang <59964154+kwakuAgyemang@users.noreply.github.com>
2022-04-14 12:57:04 +0000

    frontend changes

--- view/auth/register.php
+++ view/auth/register.php
@@ -10,79 +10,52 @@
 </head>
 <body>
     <!-- multistep form -->
-    <div class="program_container" style="display:flex;">
+<form name="registration" id="msform" action="" method='post'>
+  <!-- progressbar -->
+  <ul id="progressbar">
+    <li class="active">Account Setup</li>
+    <li>Social Profiles</li>
+    <li>Personal Details</li>
+  </ul>
+  <!-- fieldsets -->
+  <fieldset>
+    <h2 class="fs-title">Create your account</h2>
+    
+    <input type="text" name="email" placeholder="Email" required />
+    <input type="password" name="password" id="password" placeholder="Password" required />
+  
+    <input type="password" name="cpass" id="cpass" placeholder="Confirm Password" required />
+    
+    <input type="button" name="next" class="next action-button" value="Next" />
+  </fieldset>
 
-      <div class="form_side" style="width:65%">
-        <form name="registration" id="msform" action="" method='post' style="">
-          <!-- progressbar -->
-          <ul id="progressbar">
-            <li class="active">Account Setup</li>
-            <li>Personal Details</li>
-            <li>Social Profiles</li>
-          </ul>
-          <!-- fieldsets -->
-          <fieldset>
-            <h2 class="fs-title">Sign UP</h2>
-            <label for="email" style="text-align:left">Email</label>
-            <input type="text" name="email" placeholder="Email" required />
-            <label for="password">Password</label>
-            <input type="password" name="password" id="password" placeholder="Password" required />
-            <h5 id="passcheck" style="color: red;">
-              **Please Fill the password
-            </h5>
-            <label for="cpass">Confirm Password</label>
-            <input type="password" name="cpass" id="cpass" placeholder="Confirm Password" required />
-            <h5 id="cpasscheck" style="color: red;">
-                **Password didn't match
-            </h5>
-            <input type="button" name="next" class="next action-button" value="Next" />
-          </fieldset>
+  <fieldset>
+    <h2 class="fs-title">Social Profiles</h2>
+    <input type="text" name="twitter" placeholder="Twitter" />
+    <input type="text" name="facebook" placeholder="Instagram" />
+    <input type="button" name="previous" class="previous action-button" value="Previous" />
+    <input type="button" name="next" class="next action-button" value="Next" />
+  </fieldset>
 
+  <fieldset>
+    <h2 class="fs-title">Personal Details</h2>
+    <input type="text" name="fname" placeholder="First Name" required />
+    <input type="text" name="lname" placeholder="Last Name" required />
+    <input type="text" name="phone" placeholder="Phone" required />
+    <input type="number" name="age" placeholder="Age" required />
+    <input type="text" name="username" placeholder = "Username" required>
 
-
-          <fieldset>
-            <h2 class="fs-title">Personal Details</h2>
-            <label for="fname">Firstname</label>
-            <input type="text" name="fname" placeholder="First Name" required />
-            <label for="lname">Lastname</label>
-            <input type="text" name="lname" placeholder="Last Name" required />
-            <label for="phone">phone number</label>
-            <input type="text" name="phone" placeholder="Phone" required />
-            <label for="dob">Date of  Birth</label>
-            <input type="date" name="dob" placeholder="Date of Birth" required />
-            <label for="username">Username</label>
-            <input type="text" name="username" placeholder = "Username" required>
-
-            <div class="select" style="width:200px">
-              <label for="gender">Gender</label>
-              <select name="gender" id="gender" required>
-                <option value="1">Male</option>
-                <option value="2">Female</option>
-              </select>
-            </div>
-
-            <div class="select" style="width:200px">
-              <label for="sexual_orient"> Sexual Orientation</label>
-              <select name="sexual_orient" id="sexual_orient" required>
-                <option value="1">Hecterosexual</option>
-                <option value="2">Bisexual</option>
-                <option value="3">Homosexual</option>
-                <option value="4">Pansexual</option>
-                <option value="5">Pansexual</option>
-              </select>
-            </div>
-
-
-            <input type="button" name="previous" class="previous action-button" value="Previous" />
-            <input type="button" name="next" class="next action-button" value="Next" />
-          </fieldset>
-
-          <fieldset>
-            <h2 class="fs-title">Social Profiles</h2>
-            <label for="twitter">Twiter handel</label>
-            <input type="text" name="twitter" placeholder="Twitter" />
-            <label for="facebook">Facebook handel</label>
-            <input type="text" name="facebook" placeholder="Instagram" />
+    <div class="select" style="width:200px">
+      <select name="gender" id="gender" required>
+        <option value="1">Male</option>
+        <option value="2">Female</option>
+      </select>
+    </div>
+    
+    
+    <input type="button" name="previous" class="previous action-button" value="Previous" />
+    <input type="submit" name="submit" class="submit action-button" value="Submit" />
+  </fieldset>
 
             <div id="pictures_here">
               <label for="filefield">Pictures</label>
