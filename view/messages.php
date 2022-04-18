01e3532869658f7c32b9bdfe42c271f46bc52e54
rich5k <richardanatsui@gmail.com>
2022-04-18 22:46:47 +0100

    added quickblox config files

--- view/messages.php
+++ view/messages.php
@@ -9,6 +9,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link rel="stylesheet" href="../css/bootstrap.min.css">
     <link rel="stylesheet" href="../css/messages.css">
+    <link rel="stylesheet" href="../css/dashboard.css">
+    <link rel="stylesheet" href="../css/dialogs.css">
 </head>
 
 <body>
@@ -39,7 +41,7 @@
                 <div class="messages-title">
                     Messages
                 </div>
-                <div class="message row my-4">
+                <!-- <div class="message row my-4">
                     <div class="col-lg-2">
                         <img src="../images/person2.jpg" alt="match">
                     </div>
@@ -108,6 +110,48 @@
                             New Match! Say Hello 👋
                         </span>
                     </div>
+                </div> -->
+                <div class="sidebar j-sidebar">
+                    <div class="sidebar__inner">
+                        <div class="sidebar__content">
+                            <ul class="sidebar__dilog_list j-sidebar__dilog_list full">
+                                <li class="dialog__item j-dialog__item selected" id="625d95ba32eaaf0070ce62f2" data-name="akwele">
+                                    <a class="dialog__item_link" href="#!/dialog/625d95ba32eaaf0070ce62f2">
+                                        <span class="dialog__avatar m-user__img_1 m-type_chat">
+
+                                            <i class="icons">A</i>
+
+                                        </span>
+                                        <span class="dialog__info">
+                                            <span class="dialog__name">akwele</span>
+                                            <span class="dialog__last_message j-dialog__last_message">anyway, I'm not your boo so stop calling me that</span>
+                                        </span>
+                                        <span class="dialog_additional_info">
+                                            <span class="dialog__last_message_date j-dialog__last_message_date">16:47</span>
+                                            <span class="dialog_unread_counter j-dialog_unread_counter hidden"></span>
+                                        </span>
+                                    </a>
+                                </li>
+                                <li class="dialog__item j-dialog__item" id="625d826a32eaaf0070ce62db" data-name="kennyblaq">
+                                    <a class="dialog__item_link" href="#!/dialog/625d826a32eaaf0070ce62db">
+                                        <span class="dialog__avatar m-user__img_7 m-type_chat">
+
+                                            <i class="icons">K</i>
+
+                                        </span>
+                                        <span class="dialog__info">
+                                            <span class="dialog__name">kennyblaq</span>
+                                            <span class="dialog__last_message j-dialog__last_message">bro are you still there?</span>
+                                        </span>
+                                        <span class="dialog_additional_info">
+                                            <span class="dialog__last_message_date j-dialog__last_message_date">15:27</span>
+                                            <span class="dialog_unread_counter j-dialog_unread_counter hidden"></span>
+                                        </span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -125,30 +169,66 @@
                 </div>
             </div>
             <div class="chat-window ">
-                <div class="talk-bubble tri-right right-in">
-                    <!-- <span class="datetime">
-                        Sept 12, 2016 11:11PM
-                    </span> -->
-                    <div class="talktext ">
-                        <p class="from-me">
-                            Hey, we matched!
-                        </p>
-                    </div>
-                    <!-- <span class="text-status">Sent</span> -->
-                </div>
-                <div class="talk-bubble tri-right right-in">
-                    <div class="talktext">
-                        <p class="from-them">
-                            Hey, Jess. How are you?
-                        </p>
-                    </div>
+                <div class="content__inner j-content__inner">
+                    <div class="messages j-messages">
+                        <div class="message__wrap">
+                            <div class="message__content type_chat message__right ">
+                                <div class="message__text_and_date">
 
+                                    <div class="message__text_wrap you ">
+                                        <div style="height: 1px; opacity: 0;">
+                                            <p class="message__sender_name">johnedem
+                                                <samp class="message__timestamp">
+                                                    16:47
+                                                </samp>
+                                            </p>
+                                        </div>
+                                        <p class="message__text you">
+                                            Hey, we matched!
+                                        </p>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <div class="talk-bubble tri-right right-in">
+                                <div class="talktext">
+                                    <p class="from-them">
+                                        Hey, Jess. How are you?
+                                    </p>
+                                </div>
+
+                            </div>
+                            <!-- <form action="">
+                                <input type="text" placeholder="Type a message..." required>
+                                <button class="send-text" type="submit">SEND</button>
+                            </form> -->
+
+                        </div>
+                    </div>
+                    <form name="send_message" class="send_message" autocomplete="off">
+
+
+                        <div class="attachments_preview j-attachments_preview" style="display: none;"></div>
+
+                        <div class="message__actions">
+
+                            <label for="attach_btn" class="attach_btn">
+                                <i class="material-icons">attachment</i>
+                                <input type="file" id="attach_btn" name="attach_file" class="attach_input" accept="image/*">
+                            </label>
+
+                        </div>
+                        <button class="send_btn" style="top: 10px;">
+                            <img style="width: 28px;  " src="../img/Send.svg" alt="send">
+                        </button>
+                        <textarea name="message_feald" class="message_feald" id="message_feald" autocomplete="off" autocorrect="off" autocapitalize="off" placeholder="Send message…" autofocus=""></textarea>
+
+
+                    </form>
                 </div>
-                <form action="">
-                    <input type="text" placeholder="Type a message..." required>
-                    <button class="send-text" type="submit">SEND</button>
-                </form>
+
             </div>
+
         </div>
         <div class="contact-info col-lg-2">
             <div class="your-match-pics">
@@ -169,7 +249,13 @@
             </div>
         </div>
     </div>
-    <script src="https://unpkg.com/quickblox/quickblox.min.js"></script>
+    <script src="../js/QBconfig.js" defer></script>
+    <script src="../js/helpers.js" defer></script>
+    <script src="../js/user.js" defer></script>
+    <script src="../js/dialog.js" defer></script>
+    <script src="../js/message.js" defer></script>
+    <script src="../js/listeners.js" defer></script>
+    <script src="../js/app.js" defer></script>
 </body>
 
 </html>
