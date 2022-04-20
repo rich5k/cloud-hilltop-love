2efb18451faefe7ce8e7959bc5445cfe97ae453a
kwakuAgyemang <59964154+kwakuAgyemang@users.noreply.github.com>
2022-04-20 13:38:18 +0000

    register image

--- action/authprocess.php
+++ action/authprocess.php
@@ -42,6 +42,30 @@ if(isset($_POST['register'])){
         // var_dump(add_user_controller($fname, $lname, $username, $email, $pass, $twitter, $instagram, $gender, $class, $sexual_orientation, $dob, $major, $phone));
         // die;
         if( add_user_controller($fname, $lname, $username, $email, $pass, $twitter, $instagram, $gender, $class, $sexual_orientation, $dob, $major, $phone) !== true) 
+            if (isset($_FILES["file"]["name"])){
+                //Get Image Upload path
+                $targetDir = "../assets/avis/";
+                $fileName = basename($_FILES["file"]["name"]);
+                $targetFilePath = $targetDir . $fileName;
+        
+                //Get file type
+                $fileType = pathinfo($targetFilePath,PATHINFO_EXTENSION);
+        
+                //Check if file is an image and upload it to the server
+                $allowTypes = array('jpg', 'JPG', 'png','jpeg','JPEG', 'PNG');
+        
+                if(in_array($fileType, $allowTypes)){
+        
+                    // Upload file to server
+                    if(move_uploaded_file($_FILES["file"]["tmp_name"], $targetFilePath)){
+                        //echo "true";
+                        add_image_controller($fileName);
+                        $_SESSION['avi'] = $fileName;
+                        header('Location: ../view/profile.php');
+                            
+                    }
+                }
+            }
             header('Location: ../view/auth/register.php?error=Data could not be inserted');
         //$_SESSION['Uid'] = find_user_id($email);
         header("Location: ../view/auth/login.php"); 
@@ -66,7 +90,9 @@ if(isset($_POST['login'])){
             $role = $user['user_role'];
             //var_dump($role);
             if($role === '1' ){
-                header("Location: ../index.php");
+                $image = getAllUserImages($user['Uid']);
+                $_SESSION['avi'] = $image;
+                header("Location: ../view/profile.php");
             }else if ($role === '0'){
                 $_SESSION['user_role'] = 0;
                 header("Location: ../admin/index.php");
--- view/auth/register.php
+++ view/auth/register.php
@@ -17,153 +17,117 @@
 
 
 </head>
-<body style="background-color: #0f73d9;">
-    <!-- multistep form -->
-    <div class="program_container" style="display:flex;">
 
-      <div class="form_side" style="width:65%">
-        <form name="registration" id="msform" action="../../action/authprocess.php" method='post' >
-          <!-- progressbar -->
-          <ul id="progressbar">
-            <li class="active">Account Setup</li>
-            <li>Personal Details</li>
-            <li>Social Profiles</li>
-          </ul>
-          <!-- fieldsets -->
-          <fieldset id='userdetails' class='fieldset'>
-            <h2 class="fs-title">Sign UP</h2>
-            <div class='form-group'>
-              <label for="email" >Email</label>
-              <input type="email" id="email" name="email" placeholder="Email" required/>
-            
-            </div>
-            
-            <div class="form-group">
-              <label for="password">Password</label>
-              <input type="password" name="password" id="password" placeholder="Password" required />
-            
-            </div>
-            
-            <div class="form-group">
-              <label for="cpass">Confirm Password</label>
-              <input type="password"  name="cpass" id="cpass" placeholder="Confirm Password"  required/>
-            
-            </div>
-            
-            <input type="button" name="next" id="next" class="next action-button" value="Next" />
-          </fieldset>
+<body>
+  <!-- multistep form -->
+  <div class="program_container" style="display:flex;">
+
+    <div class="form_side" style="width:65%">
+      <form name="registration" id="msform" action="../../action/authprocess.php" method='post' enctype="multipart/form-data">
+        <!-- progressbar -->
+        <ul id="progressbar">
+          <li class="active">Account Setup</li>
+          <li>Personal Details</li>
+          <li>Social Profiles</li>
+        </ul>
+        <!-- fieldsets -->
+        <fieldset>
+          <h2 class="fs-title">Sign UP</h2>
+          <label for="email" style="text-align:left">Email</label>
+          <input type="text" id="email" name="email" placeholder="Email" required />
+          <h5 id="cemailcheck" style="color: red;" hidden>
+            **email should be like the ashesi email
+          </h5>
+          <label for="password">Password</label>
+          <input type="password" name="password" id="password" placeholder="Password" required />
+          <h5 id="passcheck" style="color: red;" hidden>
+            **Please Fill the password
+          </h5>
+          <label for="cpass">Confirm Password</label>
+          <input type="password" name="cpass" id="cpass" placeholder="Confirm Password" required />
+          <h5 id="cpasscheck" style="color: red;" hidden>
+            **Password didn't match
+          </h5>
+          <input type="button" name="next" class="next action-button" value="Next" />
+        </fieldset>
 
 
 
-          <fieldset id='personal_details' class='fieldset'>
-            <h2 class="fs-title">Personal Details</h2>
-            <div class="form-group">
-              <label for="fname">Firstname</label>
-              <input type="text" id="fname" name="fname" placeholder="First Name" required />
-            </div>
-            
-              
+        <fieldset>
+          <h2 class="fs-title">Personal Details</h2>
+          <label for="fname">Firstname</label>
+          <input type="text" id="fname" name="fname" placeholder="First Name" required />
+          <label for="lname">Lastname</label>
+          <input type="text" id="lname" name="lname" placeholder="Last Name" required />
+          <label for="phone">phone number</label>
+          <input type="text" id="phone" name="phone" placeholder="Phone" required />
+          <label for="dob">Date of Birth</label>
+          <input type="date" id="dob" name="dob" placeholder="Date of Birth" required />
+          <label for="username">Username</label>
+          <input type="text" id="username" name="username" placeholder="Username" required>
 
-            <div class="form-group">
-              <label for="lname">Lastname</label>
-              <input type="text" id="lname" name="lname" placeholder="Last Name" required />
-            </div>
+          <div class="select" style="width:200px">
+            <label for="gender">Gender</label>
+            <select name="gender" id="gender" required>
+              <option value="f">Male</option>
+              <option value="m">Female</option>
+            </select>
+          </div>
+          
+          <!-- <div class="select" style="width: 200px;">
+            <label for="">Class</label>
+            <select name="class" id="class" required>
+              <option value="2022">2022</option>
+              <option value="2023">2023</option>
+              <option value="2024">2024</option>
+              <option value="2025">2025</option>
+            </select>
+          </div>   -->
 
-            
-            <div class="form-group">
-              <label for="phone">phone number</label>
-              <input type="text" id="phone" name="phone" placeholder="Phone" required />
-            </div>
-
-            
-            <div class="form-group">
-              <label for="dob">Date of  Birth</label>
-              <input type="date" id="dob" name="dob" placeholder="Date of Birth" required />
-            </div>
-           
-            <div class="form-group">
-              <label for="username">Username</label>
-              <input type="text" id="username" name="username" placeholder = "Username" required>
-            </div>
-
-            
-
-            <div class='form-greoup'>
-              <div class="select" style="width:200px">
-                <label for="gender">Gender</label>
-                <select name="gender" id="gender" required>
-                  <option value="1">Male</option>
-                  <option value="2">Female</option>
-                </select>
-              </div>
-            </div>
-            
-            <div class='form-group'>
-              <div class="select" style="width:200px">
-                <label for="sexual_orient"> Sexual Orientation</label>
-                <select name="sexual_orient" id="sexual_orient" required>
-                  <option value="1">Hecterosexual</option>
-                  <option value="2">Bisexual</option>
-                  <option value="3">Homosexual</option>
-                  <option value="4">Pansexual</option>
-                </select>
-              </div>
-            </div>
-            
+          <div class="select" style="width:200px">
+            <label for="sexual_orient"> Sexual Orientation</label>
+            <select name="sexual_orientation" id="sexual_orient" required>
+              <option value="1">Heterosexual</option>
+              <option value="2">Bisexual</option>
+              <option value="3">Homosexual</option>
+              <option value="4">Pansexual</option>
+            </select>
+          </div>
 
 
-            <input type="button" name="previous" class="previous action-button" value="Previous" />
-            <input type="button" name="next" id="next" class="next action-button" value="Next" />
-          </fieldset>
+          <input type="button" name="previous" class="previous action-button" value="Previous" />
+          <input type="button" name="next" class="next action-button" value="Next" />
+        </fieldset>
 
-          <fieldset id='social_details' class='fieldset'>
-            <h2 class="fs-title">Social Profiles</h2>
-            <div class='form-group'>
-              <label for="class">Year Group</label>
-              <input type="text" id="class" name="class" placeholder="year group" />
-            </div>
+        <fieldset>
+          <h2 class="fs-title">Social Profiles</h2>
+          <label for="twitter">Twiter handle</label>
+          <input type="text" id="twitter" name="twitter" placeholder="Twitter" />
+          <label for="facebook">Intagram handle</label>
+          <input type="text" id="facebook" name="instagram" placeholder="Instagram" />
 
-            <div class='form-group'>
-              <label for="major">Offering Major</label>
-              <input type="text" id="major" name="major" placeholder="major" />
-            </div>
-            <div class='form-group'>
-              <label for="twitter">Twiter handle</label>
-              <input type="text" id="twitter" name="twitter" placeholder="Twitter" />
-            </div>
-            
-            <div class='form-group'>
-              <label for="facebook">Instagram handle</label>
-              <input type="text" id="facebook" name="facebook" placeholder="Instagram" />
-            </div>
-
-            <div class='form-group'>
-              <div id="pictures_here">
-                <label for="filefield">Pictures</label>
-                <input type="file" name="filefield" id='filefield' multiple="multiple" onchange="preview_image()">
-                <div id="image_preview" style="display:flex;"></div>
-              </div>
-            </div>
-            
-            
-            
+          <div id="pictures_here">
+            <label for="filefield">Pictures</label>
+            <input type="file" name="filefield" multiple="multiple">
+          </div>
 
 
-            <input type="button" name="previous" class="previous action-button" value="Previous" />
-            <input type="submit" name="submit" class="submit action-button" value="Submit"  onclick="registerUser();"/>
-          </fieldset>
+          <input type="button" name="previous" class="previous action-button" value="Previous" />
+          <input type="submit" name="register" class="submit action-button" value="register" onclick="registerUser();" />
+        </fieldset>
 
 
-          <script src="http://code.jquery.com/jquery-3.4.1.min.js"></script>
-          <!--<script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>-->
-        
+        <script src="http://code.jquery.com/jquery-3.4.1.min.js"></script>
+        <script src="http://code.jquery.com/ui/1.12.1/jquery-ui.min.js"> </script>
+        <script src="vendor/jquery-validation/dist/jquery.validate.min.js"></script>
 
-         
-        </form>
+        <script src='../../js/script.js'>
+        </script>
+      </form>
 
-      </div>
-      <div class="picture side" style="width:35% ">
-        <img src="../../images/sky.jpg" alt="hill top love" style="width: 568px;
+    </div>
+    <div class="picture side" style="width:35% ">
+      <img src="../../images/forall_pic.jpg" alt="hill top love" style="width: 568px;
 height: 100%;">
 
     </div>
