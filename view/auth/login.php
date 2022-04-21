59441164e431e7af6b0441e55356013282626db8
Richard Anatsui <richardanatsui@gmail.com>
2022-04-21 01:24:13 +0100

    fixed login syntax errors

--- view/auth/login.php
+++ view/auth/login.php
@@ -1,77 +1,78 @@
 <?php
 session_start();
 
-  if(isset($_SESSION['errors'])){
-    $error ="<div class = 'error'>
+if (isset($_SESSION['errors'])) {
+  $error = "<div class = 'error'>
               Email or password is incorrect
               </div>";
-  }
-  if(isset($_SESSION['error'])){
-    $errors ="<div class = 'error'>
+}
+if (isset($_SESSION['error'])) {
+  $errors = "<div class = 'error'>
               Password is incorrect
               </div>";
-             
-  }
-  
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link rel="stylesheet" href="../../css/style_login.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href=" https://use.fontawesome.com/releases/v5.8.1/css/all.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js">
-</head>
-<body style="background-color: #1f7ce3;">
-    <!-- multistep form -->
-    <div class="program_container" style="display:flex;">
 
-      <div class="form_side" style="width:65%">
-        <form name="registration" id="msform" action="../../action/authprocess.php" method='post' style="" style="margin-left:auto margin-right:auto">
-          <!-- progressbar here -->
-          <fieldset>
-            <h2 class="fs-title" style="color:">Sign In</h2>
-            <?php if(isset($error))
-                  echo $error;
-                  if(isset($errors))
-                  echo $error ?>
-            <div class="form-group">
-              <label for="email">Email</label>
-              <input type="text" name="email" placeholder="Email" required />
-            </div>
-            
-            <div class="form-group">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login</title>
+  <link rel="stylesheet" href="../../css/style_login.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href=" https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js">
+</head>
+
+<body style="background-color: #1f7ce3;">
+  <!-- multistep form -->
+  <div class="program_container" style="display:flex;">
+
+    <div class="form_side" style="width:65%">
+      <form name="registration" id="msform" action="../../action/authprocess.php" method='post' style="margin-left:auto; margin-right:auto;">
+        <!-- progressbar here -->
+        <fieldset>
+          <h2 class="fs-title">Sign In</h2>
+          <?php if (isset($error))
+            echo $error;
+          if (isset($errors))
+            echo $error ?>
+          <div class="form-group">
+            <label for="email">Email</label>
+            <input type="text" name="email" placeholder="Email" required />
+          </div>
+
+          <div class="form-group">
             <label for="password">Password</label>
             <input type="password" name="password" id="password" placeholder="Password" required />
-            </div>
-            
-            <span>
-              <input type="submit" name="signin" class="submit action-button" value="Login" onclick="loginUser();">
-              <p class='text'>Dont have an Account? <a href="./register.php">Sign Up here</a></p>
-            </span> 
-            
-          </fieldset>
           </div>
-          </form>
-          <div class="picture side" style="width:50% ">
-        <img src="../../images/sky.jpg" alt="hill top love" style="width: 714px; height: 730px">
-      </div>
-</div>
 
-          <script src="http://code.jquery.com/jquery-3.4.1.min.js"></script>
-        <script src="http://code.jquery.com/ui/1.12.1/jquery-ui.min.js"> </script>
-        <script src="vendor/jquery-validation/dist/jquery.validate.min.js"></script>
-        <script src="https://unpkg.com/quickblox/quickblox.min.js"></script>
-        <script src="../../js/quickblox.js"></script>
-        <script src="../../js/quickblox-auth.js"></script>
-        <script src="../../js/login_validation.js"></script>
-  
+          <span>
+            <input type="submit" name="signin" class="submit action-button" value="Login" onclick="loginUser();">
+            <p class='text'>Dont have an Account? <a href="./register.php">Sign Up here</a></p>
+          </span>
 
-  
+        </fieldset>
+    </div>
+    </form>
+    <div class="picture side" style="width:50% ">
+      <img src="../../images/sky.jpg" alt="hill top love" style="width: 714px; height: 730px">
+    </div>
+  </div>
+
+  <script src="http://code.jquery.com/jquery-3.4.1.min.js"></script>
+  <script src="http://code.jquery.com/ui/1.12.1/jquery-ui.min.js"> </script>
+  <script src="vendor/jquery-validation/dist/jquery.validate.min.js"></script>
+  <script src="https://unpkg.com/quickblox/quickblox.min.js"></script>
+  <script src="../../js/quickblox.js"></script>
+  <script src="../../js/quickblox-auth.js"></script>
+  <script src="../../js/login_validation.js"></script>
+
+
+
 </body>
 
 </html>
