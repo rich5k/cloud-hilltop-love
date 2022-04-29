e971c22036c3a594339ec4cd76c117712c9c8381
Richard Anatsui <richardanatsui@gmail.com>
2022-04-29 11:41:07 +0100

    added match_action and its supporting functions

---
+++ action/match_action.php
@@ -0,0 +1,25 @@
+<?php
+
+//if swiped yes populate user_like table
+if (isset($_POST["love"])) {
+
+    //get username of both parties.
+    $likee_username = $_GET['liked_users_id'];
+    $liker_username = $_SESSION['Uid'];
+
+    //populate user likes table with who you like.
+    $record_like = recordLikeController($likee_username,  $liker_username);
+
+    //match algo
+    // check if there exists a record with both id but with likee in the position of liker
+
+    $check_for_match = checkMatchController($likee_username,  $liker_username);
+
+    if ($check_for_match == true) {
+        // insert record into match table
+        record_success_match_controller($likee_username,  $liker_username);
+        // send successful match notification
+        // header("Location: ../view/auth/login.php");     
+        exit;
+    }
+}
--- controllers/UserController.php
+++ controllers/UserController.php
@@ -1,79 +1,112 @@
 <?php
 require("../classes/User.php");
 
-function add_user_controller($fname, $lname, $username, $email, $pass,  $gender, $twitter, $instagram, $class, $sexual_orientation, $dob, $major, $phone){
+function add_user_controller($fname, $lname, $username, $email, $pass,  $gender, $twitter, $instagram, $class, $sexual_orientation, $dob, $major, $phone)
+{
     $user_instance = new user();
     return $user_instance->adduser($fname, $lname, $username, $email, $pass,  $gender, $twitter, $instagram, $class, $sexual_orientation, $dob, $major, $phone);
 }
 
-function delete_user_controller($id){
+function delete_user_controller($id)
+{
     $user_instance = new user();
     return $user_instance->deleteuser($id);
 }
 
-function update_user_controller($id,$fname, $lname, $username, $email, $pass, $twitter, $instagram, $gender, $class, $sexual_orientation, $dob, $major, $phone){
+function update_user_controller($id, $fname, $lname, $username, $email, $pass, $twitter, $instagram, $gender, $class, $sexual_orientation, $dob, $major, $phone)
+{
     $user_instance = new user();
-    return $user_instance->edituser($id,$fname, $lname, $username, $email, $pass, $twitter, $instagram, $gender, $class, $sexual_orientation, $dob, $major, $phone);
+    return $user_instance->edituser($id, $fname, $lname, $username, $email, $pass, $twitter, $instagram, $gender, $class, $sexual_orientation, $dob, $major, $phone);
 }
 
-function find_user_controller($email){
+function find_user_controller($email)
+{
     $user_instance = new user();
     return $user_instance->finduser($email);
 }
 
-function find_email_controller($email) {
+function find_email_controller($email)
+{
     $user_instance = new user();
     return $user_instance->findEmail($email);
 }
 
-function find_user_id_controller($email) {
+function find_user_id_controller($email)
+{
     $user_instance = new user();
     return $user_instance->findID($email);
 }
 
-function add_image_controller($Uid, $image_1){
+function add_image_controller($Uid, $image_1)
+{
     $user_instance = new user();
     return $user_instance->addImage($Uid, $image_1);
 }
 
-function update_image_controller($id,$image_1){
+function update_image_controller($id, $image_1)
+{
     $user_instance = new User();
-    return $user_instance->updateImage($id,$image_1);
+    return $user_instance->updateImage($id, $image_1);
 }
 
-function get_user_controller($id){
+function get_user_controller($id)
+{
     $user_instance = new user();
     return $user_instance->getUser($id);
 }
 
-function get_partner_controller($id, $gender, $sexual_orientation){
+function get_partner_controller($id, $gender, $sexual_orientation)
+{
     $user_instance = new User();
     return $user_instance->getPartner($id, $gender, $sexual_orientation);
 }
 
 
-function get_all_user_images_controller($id){
+function get_all_user_images_controller($id)
+{
     $user_instance = new User();
     return $user_instance->getAllUserImages($id);
 }
 
-function get_user_messages($id){
+function get_user_messages($id)
+{
     $user_instance = new User();
     return $user_instance->getUserMessages($id);
 }
 
-function get_likes($id){
+function get_likes($id)
+{
     $user_instance = new User();
     return $user_instance->getUserLikes($id);
 }
 
-function add_matches($id, $lid){
+function add_matches($id, $lid)
+{
     $user_instance = new User();
-    return $user_instance->addMatches($id,$lid);
+    return $user_instance->addMatches($id, $lid);
 }
 
-function get_Number_of_Likes($id){
+function get_Number_of_Likes($id)
+{
     $user_instance = new User();
     return $user_instance->getNumberofLikes($id);
+}
 
+function recordLikeController($likee_username,  $liker_username)
+{
+    $user_instance = new User();
+    return $user_instance->recordLike($likee_username,  $liker_username);
+}
+
+
+function checkMatchController($likee_username,  $liker_username)
+{
+    $user_instance = new User();
+    return $user_instance->recordLike($likee_username,  $liker_username);
+}
+
+function record_success_match_controller($likee_username,  $liker_username)
+{
+    $user_instance = new User();
+    return $user_instance->record_success_match($likee_username,  $liker_username);
 }
