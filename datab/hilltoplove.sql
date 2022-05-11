958f50ca962ed5136f6ff77e172b1e8c4555c6ca
kwakuAgyemang <59964154+kwakuAgyemang@users.noreply.github.com>
2022-05-11 01:24:00 +0000

    updated database

--- datab/hilltoplove.sql
+++ datab/hilltoplove.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 06, 2022 at 07:19 PM
--- Server version: 10.4.16-MariaDB
--- PHP Version: 7.4.12
+-- Generation Time: May 11, 2022 at 03:22 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -67,9 +67,18 @@ INSERT INTO `courses` (`course_id`, `course_name`, `course_title`) VALUES
 
 CREATE TABLE `interest` (
   `int_id` int(8) NOT NULL,
-  `interest_name` varchar(50) DEFAULT NULL,
-  `Uid` int(8) DEFAULT NULL
+  `interest_name` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `interest`
+--
+
+INSERT INTO `interest` (`int_id`, `interest_name`) VALUES
+(1, 'Volunteering'),
+(2, 'Sports'),
+(3, 'Swimming'),
+(4, 'Eating');
 
 -- --------------------------------------------------------
 
@@ -91,7 +100,8 @@ INSERT INTO `pictures` (`pic_id`, `Uid`, `pic_name`) VALUES
 (4, 29, 'part1.6.png'),
 (5, 30, 'close-studio-portrait-beautiful-young-260nw-649133968.png'),
 (37, 64, 'github pic.jpg'),
-(38, 65, 'Sayoni Discord.PNG');
+(38, 65, 'Sayoni Discord.PNG'),
+(39, 10, 'results1.png');
 
 -- --------------------------------------------------------
 
@@ -142,7 +152,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`Uid`, `fname`, `lname`, `username`, `email`, `pass`, `gender`, `dob`, `class`, `major`, `phone`, `twitter`, `insta`, `sexual_orientation`) VALUES
-(10, 'Kwaku', 'Agyeman', 'AgyemanK', 'kwaku.agyeman@ashesi.edu.gh', '$2y$10$JdTIlCoRNVR5flk/uA4YyO3Hz3W0YqT9miEojZobH4fUoHJPR/4Ku', 'm', '2020-05-14', 2022, 3, '0246000307', '@agyeman', '@sdfjksa', 1),
+(10, 'Kwaku', 'Agyeman', 'AgyemanKwaku', 'kwaku.agyeman@ashesi.edu.gh', '', 'm', '2000-12-13', 2022, 3, '0246000307', '@agyeman', '@sdfjksa', 1),
 (29, 'Kofi', 'Omari', 'OmariKofi', 'kwakuayemang.2000@gmail.com', '$2y$10$ZX9Tsfl6T.XOeL2dEp5ZWes4B2vsXucC/pYER.tPqolddr2ngjqeq', 'm', '2002-02-05', 2003, 4, '0243923841', '@kwame_in', '@sdfjksdf', 1),
 (30, 'Afia', 'Ohenewaa', 'OhenewaaAfia', 'ohenewaaAfia@ashesi.edu.gh', 'oiodas23904jk@8q34#8934', 'f', '2000-01-01', 2003, 5, '0273982120', '@ohenewaa', 'afia_ohenewaa', 1),
 (64, 'Richard', 'Anatsui', 'rich5k', 'richard.anatsui@ashesi.edu.gh', '$2y$10$qAKA7EBxVZb5IVXLXrFcbejxeV6tzEN2.mMi5Mo7uufwiAL0m/dRu', 'm', '2001-06-07', 2004, 2, '+233205030128', '@richard', 'https://www.instagram.com/richardkafuianatsui/', 1),
@@ -181,7 +191,9 @@ INSERT INTO `user_likes` (`like_id`, `Uid`, `Iid`, `like_dis`) VALUES
 (84, 64, 30, 'd'),
 (85, 64, 65, 'l'),
 (86, 65, 29, 'd'),
-(87, 65, 64, 'l');
+(87, 65, 64, 'l'),
+(95, 10, 30, 'd'),
+(96, 10, 65, 'l');
 
 -- --------------------------------------------------------
 
@@ -224,8 +236,7 @@ ALTER TABLE `courses`
 -- Indexes for table `interest`
 --
 ALTER TABLE `interest`
-  ADD PRIMARY KEY (`int_id`),
-  ADD KEY `Uid` (`Uid`);
+  ADD PRIMARY KEY (`int_id`);
 
 --
 -- Indexes for table `pictures`
@@ -292,13 +303,13 @@ ALTER TABLE `courses`
 -- AUTO_INCREMENT for table `interest`
 --
 ALTER TABLE `interest`
-  MODIFY `int_id` int(8) NOT NULL AUTO_INCREMENT;
+  MODIFY `int_id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `pictures`
 --
 ALTER TABLE `pictures`
-  MODIFY `pic_id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+  MODIFY `pic_id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 
 --
 -- AUTO_INCREMENT for table `sexual_orientation`
@@ -322,7 +333,7 @@ ALTER TABLE `user_interest`
 -- AUTO_INCREMENT for table `user_likes`
 --
 ALTER TABLE `user_likes`
-  MODIFY `like_id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=95;
+  MODIFY `like_id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=97;
 
 --
 -- AUTO_INCREMENT for table `user_match`
@@ -340,12 +351,6 @@ ALTER TABLE `user_match`
 ALTER TABLE `converstion`
   ADD CONSTRAINT `converstion_ibfk_1` FOREIGN KEY (`sender_id`) REFERENCES `users` (`Uid`),
   ADD CONSTRAINT `converstion_ibfk_2` FOREIGN KEY (`receiver_id`) REFERENCES `users` (`Uid`);
-
---
--- Constraints for table `interest`
---
-ALTER TABLE `interest`
-  ADD CONSTRAINT `interest_ibfk_1` FOREIGN KEY (`Uid`) REFERENCES `users` (`Uid`);
 
 --
 -- Constraints for table `pictures`
